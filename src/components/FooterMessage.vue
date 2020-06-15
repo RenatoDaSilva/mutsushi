bc2471cd66290196f96caa0f465542882a338d66
Renato da Silva <renato.katia@gmail.com>
2020-06-14 21:27:43 -0300

    structures the code

---
+++ src/components/FooterMessage.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="panel-footer">
+    <div class="table-responsive">
+      <table class="table">
+        <tbody>
+          <tr>
+            <td>
+              <a href="https://www.instagram.com/mutsushidelivery/?hl=pt-br">
+                <img
+                  class="img-responsive"
+                  src="../assets/insta_conta.png"
+                  alt="Instagram"
+                  style="float:right;width:300px"
+                />
+              </a>
+            </td>
+            <td>
+              <a
+                href="https://api.whatsapp.com/send?phone=5548996761473&text=Oii+estou+vindo+do+Site+&#128513;"
+              >
+                <img
+                  class="img-responsive"
+                  src="../assets/wpp_numero.png"
+                  alt="WhatsApp"
+                  style="float:left;width:300px"
+                />
+              </a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FooterMessage"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.panel-footer {
+  position: sticky;
+  bottom: 0;
+  height: 80px;
+}
+</style>
---
+++ src/components/HeaderMessage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="panel-heading">
+    <img class="logo-heading" src="../assets/logo.jpg" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderMessage"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.panel-heading {
+  position: sticky;
+  top: 0;
+}
+
+.logo-heading {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 400px;
+  width: 100%;
+}
+</style>
