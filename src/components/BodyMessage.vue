5d452bc21289e886e2f05a0317104b5d5374760e
renato <renato.katia@gmail.com>
2020-10-29 23:41:59 -0300

    Removes nav buttons and changes transition type

--- src/components/BodyMessage.vue
+++ src/components/BodyMessage.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
-    <vue-flux :options="vfOptions" :images="vfImages" :transitions="vfTransitions" ref="slider">
+    <vue-flux
+      :options="vfOptions"
+      :images="vfImages"
+      :transitions="vfTransitions"
+      ref="slider"
+    >
       <template v-slot:preloader>
         <flux-preloader />
-      </template>
-
-      <template v-slot:controls>
-        <flux-controls />
       </template>
 
       <template v-slot:pagination>
@@ -17,14 +18,13 @@
 </template>
 
 <script>
-import { VueFlux, FluxControls, FluxPagination, FluxPreloader } from "vue-flux";
+import { VueFlux, FluxPagination, FluxPreloader } from "vue-flux";
 import List from "../services/listaImg";
 
 export default {
   name: "BodyMessage",
   components: {
     VueFlux,
-    FluxControls,
     FluxPagination,
     FluxPreloader,
   },
@@ -34,12 +34,11 @@ export default {
         autoplay: true,
       },
       vfImages: [],
-      vfTransitions: ["slide"],
+      vfTransitions: ["fade"],
     };
   },
   mounted() {
     List.getList().then((res) => {
-      console.log(res.data);
       this.vfImages = res.data.carousel;
     });
   },
